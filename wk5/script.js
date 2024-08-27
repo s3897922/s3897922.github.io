28472a53b519d84be7a064d2db68edb12a09120f
s3897922 <s3897922@student.rmit.edu.au>
2024-08-27 16:24:18 +1000

    script

--- wk5/script.js
+++ wk5/script.js
@@ -1,1 +1,8 @@
 console.log("welcome to interactive media");
+let count = 10;
+console.log(count);
+const name = "Sarah";
+console.log("Hello", name);
+const myName = "Min";
+const myCity = "Melbourne";
+console.log(myName, "lives in", myCity);
