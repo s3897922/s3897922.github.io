f7f45deef70108e3f0a354c6f20d456ee8eabdb7
s3897922 <s3897922@student.rmit.edu.au>
2024-09-10 14:18:44 +1000

    calculation

---
+++ wk6/script.js
@@ -0,0 +1,9 @@
+function checkGrade() {
+  const a1 = document.querySelector("#answer1");
+  console.log(a1.value);
+  const a2 = document.querySelector("#answer2");
+  console.log(a2.value);
+  // let total = a1.value + a2.value;
+  let total = parseFloat(a1.value) + parseFloat(a2.value);
+  console.log("sum is", total);
+}
