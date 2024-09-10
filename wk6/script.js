07cbdf12b03530c82fb55d252f0dea0e8047266e
s3897922 <s3897922@student.rmit.edu.au>
2024-09-10 14:31:36 +1000

    sendReport function

--- wk6/script.js
+++ wk6/script.js
@@ -4,11 +4,27 @@ function checkGrade() {
   const a2 = document.querySelector("#answer2");
   console.log(a2.value);
   // let total = a1.value + a2.value;
-  let total = parseFloat(a1.value) + parseFloat(a2.value);
-  console.log("sum is", total);
+  let a1value = parseFloat(a1.value);
+  let a2value = parseFloat(a2.value);
+  let sum = calculateTotal(a1value, a2value);
+  //   let sum = parseFloat(a1.value) + parseFloat(a2.value);
+  console.log("sum is", sum);
+  sendReport(sum);
 }
 
 function calculateTotal(a, b) {
   let total = a + b;
   return total;
+}
+
+function sendReport(score) {
+  const report = document.querySelector("#report");
+  console.log(report);
+  if (score > 30) {
+    console.log("you got HD");
+    report.textContent = "you got HD";
+  } else if (score > 20 && score <= 30) {
+    console.log("you got a D");
+    report.textConcent = "you got a D";
+  }
 }
