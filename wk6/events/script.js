9ff0da0f182fd7553b021d234b34334c68000b60
s3897922 <s3897922@student.rmit.edu.au>
2024-09-17 14:02:39 +1000

    click count

--- wk6/events/script.js
+++ wk6/events/script.js
@@ -3,6 +3,12 @@ console.log(myButton);
 
 myButton.addEventListener("click", myFunction);
 
+let clickCount = 0;
+const count = document.querySelector("#count");
+
 function myFunction() {
   console.log("het did you just click me?");
+  //   clickCount = clickCount +1;
+  clickCount++;
+  count.textConcent = clickCount;
 }
