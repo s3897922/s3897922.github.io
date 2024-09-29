a13bc8ffd43d4e050b6f1f5e412ceb950a462e7b
s3897922 <s3897922@student.rmit.edu.au>
2024-09-29 17:53:21 +1000

    replay function working

--- assignment2/script.js
+++ assignment2/script.js
@@ -1,11 +1,12 @@
 const audio = document.querySelector("#custom-audio-player");
 const audioPlayPauseBtn = document.querySelector("#audio-play-pause-btn");
 const audioPlayPauseImg = document.querySelector("#audio-play-pause-img");
+const audioReplayBtn = document.querySelector("#audio-replay-btn");
 const audioProgressBar = document.querySelector("#audio-progress-bar-fill");
 audio.removeAttribute("controls");
 
 audioPlayPauseBtn.addEventListener("click", toggleAudioPlayPause);
-
+audioReplayBtn.addEventListener("click", replayAudio);
 audio.addEventListener("timeupdate", updateAudioProgressBar);
 function toggleAudioPlayPause() {
   if (audio.paused || audio.ended) {
@@ -21,15 +22,22 @@ function updateAudioProgressBar() {
   const value = (audio.currentTime / audio.duration) * 100;
   audioProgressBar.style.width = value + "%";
 }
+function replayAudio() {
+  audio.currentTime = 0;
+  audio.play();
+  audioPlayPauseImg.src = "https://img.icons8.com/ios-glyphs/30/pause--v1.png"; // Update play/pause button to show pause icon
+}
+
 // Add other functionalities here
 const video = document.querySelector("#custom-video-player");
 const videoPlayPauseBtn = document.querySelector("#video-play-pause-btn");
 const videoPlayPauseImg = document.querySelector("#video-play-pause-img");
+const videoReplayBtn = document.querySelector("#video-replay-btn");
 const videoProgressBar = document.querySelector("#video-progress-bar-fill");
 video.removeAttribute("controls");
 
 videoPlayPauseBtn.addEventListener("click", toggleVideoPlayPause);
-
+videoReplayBtn.addEventListener("click", replayVideo);
 video.addEventListener("timeupdate", updateVideoProgressBar);
 function toggleVideoPlayPause() {
   if (video.paused || video.ended) {
@@ -44,4 +52,9 @@ function toggleVideoPlayPause() {
 function updateVideoProgressBar() {
   const value = (video.currentTime / video.duration) * 100;
   videoProgressBar.style.width = value + "%";
+}
+function replayVideo() {
+  video.currentTime = 0;
+  video.play();
+  videoPlayPauseImg.src = "https://img.icons8.com/ios-glyphs/30/pause--v1.png"; // Update play/pause button to show pause icon
 }
