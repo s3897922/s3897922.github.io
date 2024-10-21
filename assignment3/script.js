e8bc086703e50ce3f6515931136c226727c44a63
s3897922 <s3897922@student.rmit.edu.au>
2024-10-21 21:13:09 +1100

    updat

--- assignment3/script.js
+++ assignment3/script.js
@@ -1,4 +1,4 @@
-// let draggedElement = null;
+let draggedElement = null;
 
 // const colorBox = document.querySelector(".colorbox");
 // const dropBox = document.querySelector(".dropbox");
@@ -6,7 +6,7 @@
 // colorBox.addEventListener("dragstart", startDrag);
 
 // function startDrag() {
-//   draggedElement = colorBox;
+// draggedElement = colorBox;
 // }
 
 // dropBox.addEventListener("dragover", endDrag);
@@ -18,13 +18,13 @@
 // dropBox.addEventListener("drop", handleDrop);
 
 // function handleDrop() {
-//   if (draggedElement) {
-//     const color = window
-//       .getComputedStyle(draggedElement)
-//       .getPropertyValue("background-color");
-//     // const color = draggedElement.style.backgroundColor;
-//     dropBox.style.backgroundColor = color;
-//     dropBox.textContent = "Dropped!";
-//     draggedElement = null;
-//   }
+//    if (draggedElement) {
+//      const color = window
+//        .getComputedStyle(draggedElement)
+//        .getPropertyValue("background-color");
+//      // const color = draggedElement.style.backgroundColor;
+//      dropBox.style.backgroundColor = color;
+//      dropBox.textContent = "Dropped!";
+//      draggedElement = null;
+//    }
 // }
